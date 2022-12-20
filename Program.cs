5bed3fe8a953d751b3b3e457f873234e47db163f
BaSmAlA97 <120977520+BaSmAlA97@users.noreply.github.com>
2022-12-20 20:33:23 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z = 0;
+            Console.WriteLine("enter first number");
+            int num1=int.Parse(Console.ReadLine());
+
+            Console.WriteLine("enter second number");
+            int num2 = int.Parse(Console.ReadLine());
+
+            for(x = num1; x <= num2;x++)
+            {
+                for (y = 2; y <= sqrt(x); y++)
+                {
+                    if (x%y==0) 
+                    {
+                        z = 0;
+                    }
+                }
+                if(z==1&&x!=1)
+                {
+                    Console.WriteLine(x + "is prime");
+                }
+                z = 1;
+                
+            }
+
+
+            Console.ReadKey();
+        }
+        static double sqrt (int x) 
+        {
+            return Math.Sqrt(x);    
+        }
+    }
+}
